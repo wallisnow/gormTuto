0307f64868bcadb3ac65050bd8cf2d48d9a8d17f
wallisnow <johnwufr@gmail.com>
2022-03-30 21:33:57 +0300

    use redis

---
+++ src/RedisUtile.go
@@ -0,0 +1,37 @@
+package src
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+func NewPool() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81", // no password set
+		DB:       0,                                  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "my_value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+}
